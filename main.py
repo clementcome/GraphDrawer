d5358139f813952ac2c6c1788408a9e2935fce9d
clement <clement.come@student-cs.fr>
2019-10-15 19:49:10 +0200

    Handle saving of graphs and improved presentation

--- main.py
+++ main.py
@@ -37,13 +37,15 @@ edges = [
     )
 ]
 
-last_data = [{'data': {'id': '0', 'label': 'Resource'}, 'classes': 'round'}, {'data': {'id': '1', 'label': 'Literal'}, 'classes': 'square'}, {'data': {'source': '0', 'target': '1', 'id': '-2', 'label': 'Property'}, 'classes': 'directed'}]
+last_data = [{'data': {'id': '4', 'label': '"Bob"'}, 'classes': 'square'}, {'data': {'id': '5', 'label': '"Alice"'}, 'classes': 'square'}, {'data': {'id': '6', 'label': 'D1'}, 'classes': 'round'}, {'data': {'id': '7', 'label': 'D2'}, 'classes': 'round'}, {'data': {'id': '8', 'label': 'P1'}, 'classes': 'round'}, {'data': {'id': '9', 'label': 'P2'}, 'classes': 'round'}, {'data': {'id': '10', 'label': '"Carl"'}, 'classes': 'square'}, {'data': {'id': '11', 'label': '"David"'}, 'classes': 'square'}, {'data': {'source': '6', 'target': '5', 'id': '-1', 'label': 'name'}, 'classes': 'directed'}, {'data': {'source': '7', 'target': '4', 'id': '-2', 'label': 'name'}, 'classes': 'directed'}, {'data': {'source': '8', 'target': '10', 'id': '-3', 'label': 'name'}, 'classes': 'directed'}, {'data': {'source': '9', 'target': '11', 'id': '-4', 'label': 'name'}, 'classes': 'directed'}, {'data': {'source': '6', 'target': '9', 'id': '-5', 'label': 'treats'}, 'classes': 'directed'}, {'data': {'source': '6', 'target': '8', 'id': '-6', 'label': 'treats'}, 'classes': 'directed'}, {'data': {'source': '7', 'target': '8', 'id': '-7', 'label': 'treats'}, 'classes': 'directed'}]
 
 app.layout = html.Div([
-    html.H1("GraphDrawer"),
     html.Div([
-        "Enter a name for the new node :",
-        dcc.Input(id='input-node-name', type='text', placeholder= 'Name'),
+        html.H1("GraphDrawer"),
+        html.H2("Handling of nodes"),
+        html.Div([
+            "Enter a name for the new node :",
+            dcc.Input(id='input-node-name', type='text', placeholder= 'Name'),
         "Type of node : ",
         dcc.Dropdown(
             options = [
@@ -55,95 +57,119 @@ app.layout = html.Div([
             id = 'node-type',
             style = {'width': '200px', 'display': 'inline-block'}
         ),
-        html.Button('Create a new node', id='button-create-node')
-    ], style={'display': 'flex', 'align-items': 'center'}),
+        ]),
+        html.Button('Create a new node', id='button-create-node'),
+        html.Div([
+            "Do you want to delete this node : ",
+            html.Span(id='delete-node-name'),
+            ' ?',
+            html.Button('Yes', id='button-delete-node')
+        ]),
+        html.H2('Handling of edges'),
+        html.Div([
+            "To create a new edge, enter its name, then click the source node and after click the target node. You can tick the box if you want the edge to be directed."
+        ]),
+        html.Div([
+            "Enter a name for the new edge :",
+            dcc.Input(id='input-edge-name', type='text', placeholder= 'Name'),
+        html.Div([
+            "What type of edge do you want ?",
+            dcc.RadioItems(
+                id='edge-direction',
+                options=[
+                    {'label': 'Undirected edge', 'value': 'undirected'},
+                    {'label': 'Directed edge', 'value': 'directed'},
+                ],
+                value= 'undirected',
+                style={'border-style': 'solid', 'border-width': "1px", "margin":"2px"}
+            ),
+            dcc.RadioItems(
+                id='edge-dotted',
+                options=[
+                    {'label': 'Solid edge', 'value': 'solid'},
+                    {'label': 'Dotted edge', 'value': 'dotted'},
+                ],
+                value= 'solid',
+                style={'border-style': 'solid', 'border-width': "1px", "margin":"2px"}
+            ),
+        ], style={"margin":"5px"}),
+            "Source node :",
+            html.Span(id='source-node-name'),
+            ". Target node : ",
+            html.Span(id='target-node-name'),
+        ],),
+        html.Button('Create a new edge', id='button-create-edge'),
+        html.Div([
+            "Do you want to delete this edge : ",
+            html.Span(id='delete-edge-name'),
+            ' ?',
+            html.Button('Yes', id='button-delete-edge')
+        ]),
+        html.Div(id='timestamps'),
+        html.Button('Save json',id='save-json'),
+        html.Button('Use saved json',id='use-saved-json'),
+    ], style={"flex": "1"}),
     html.Div([
-        "Do you want to delete this node : ",
-        html.Span(id='delete-node-name'),
-        ' ?',
-        html.Button('Yes', id='button-delete-node')
-    ]),
-    html.Div([
-        "To create a new edge, enter its name, then click the source node and after click the target node. You can tick the box if you want the edge to be directed."
-    ]),
-    html.Div([
-        "Enter a name for the new edge :",
-        dcc.Input(id='input-edge-name', type='text', placeholder= 'Name'),
-        "What type of edge do you want ?",
-        dcc.RadioItems(
-            id='edge-direction',
-            options=[
-                {'label': 'Undirected edge', 'value': 'undirected'},
-                {'label': 'Directed edge', 'value': 'directed'},
-            ],
-            value= 'undirected',
-            labelStyle= {'display': 'inline-block'}
+        cyto.Cytoscape(
+            id='cytoscape-graph',
+            layout={'name': 'cose'},
+            elements = [],
+            style = {'width': '100%', 'height': '100%'},
+            stylesheet = [
+                {
+                    'selector': 'edge',
+                    'style': {
+                        'label': 'data(label)',
+                        'font-size': '12',
+                        'curve-style': 'bezier',
+                        'text-rotation': 'autorotate'
+                    }
+                },
+                {
+                    'selector': 'node',
+                    'style': {
+                        'label': 'data(label)',
+                        'font-size': '12',
+                        'border-width': '2',
+                        'background-color': 'white',
+                    }
+                },
+                {
+                    'selector':'.square',
+                    'style': {
+                        'shape': 'rectangle'
+                    }
+                },
+                {
+                    'selector':'node.dotted',
+                    'style': {
+                        'border-style': 'dashed'
+                    }
+                },
+                {
+                    'selector':'edge.dotted',
+                    'style': {
+                        'line-style': 'dashed'
+                    }
+                },
+                {
+                    'selector':'.directed',
+                    'style': {
+                        'target-arrow-shape': 'triangle',
+                    }
+                },
+            ]
         ),
-        ". Source node :",
-        html.Span(id='source-node-name'),
-        ". Target node : ",
-        html.Span(id='target-node-name'),
-        html.Button('Create a new edge', id='button-create-edge')
-    ], style={'display': 'flex', 'align-items': 'center'}),
-    html.Div([
-        "Do you want to delete this edge : ",
-        html.Span(id='delete-edge-name'),
-        ' ?',
-        html.Button('Yes', id='button-delete-edge')
-    ]),
-    html.Div(id='timestamps'),
-    html.Button('Print json',id='print-json'),
-    cyto.Cytoscape(
-        id='cytoscape-graph',
-        layout={'name': 'cose'},
-        elements = [],
-        style = {'width': '100%', 'height': '600px'},
-        stylesheet = [
-            {
-                'selector': 'edge',
-                'style': {
-                    'label': 'data(label)',
-                    'curve-style': 'bezier',
-                    'text-rotation': 'autorotate'
-                }
-            },
-            {
-                'selector': 'node',
-                'style': {
-                    'label': 'data(label)',
-                    'border-width': '2',
-                    'background-color': 'white',
-                }
-            },
-            {
-                'selector':'.square',
-                'style': {
-                    'shape': 'rectangle'
-                }
-            },
-            {
-                'selector':'.dotted',
-                'style': {
-                    'border-style': 'dashed'
-                }
-            },
-            {
-                'selector':'.directed',
-                'style': {
-                    'target-arrow-shape': 'triangle',
-                }
-            },
-        ]
-    ),
-    html.Div(json.dumps(last_data) , id='store-data', style={'display':'none'}),
-    html.Div(id='delete-node-id', style={'display': 'none'}),
-    html.Div(id='delete-edge-id', style={'display': 'none'}),
-    html.Div(id='source-node-id', style={'display': 'none'}),
-    html.Div(id='target-node-id', style={'display': 'none'}),
-    html.Div(id='next-node-id', style={'display': 'none'}),
-    html.Div(id='next-edge-id', style={'display': 'none'}),
-    html.Div(id='useless', style={'display': 'none'}),
-])
+        html.Pre(id='useless', style={'display': 'none'}),
+        html.Div(json.dumps([]) , id='store-data', style={'display':'none'}),
+        html.Div(id='delete-node-id', style={'display': 'none'}),
+        html.Div(id='delete-edge-id', style={'display': 'none'}),
+        html.Div(id='source-node-id', style={'display': 'none'}),
+        html.Div(id='target-node-id', style={'display': 'none'}),
+        html.Div(id='next-node-id', style={'display': 'none'}),
+        html.Div(id='next-edge-id', style={'display': 'none'}),
+    ], style={"flex": "1"})
+],style={"display": "flex"})
 
 # Utils
 def button_was_fired(ts, li_other_ts):
@@ -184,13 +210,22 @@ def display_timestamps(*args):
 
 @app.callback(
     Output('useless','children'),
-    [Input('print-json','n_clicks')],
-    [State('store-data', 'children')]
+    [Input('save-json','n_clicks')],
+    [State('next-node-id','children'),
+    State('next-edge-id','children'),
+    State('store-data', 'children')]
 )
-def print_json(n_clicks, stored_json):
-    stored_data = json.loads(stored_json)
-    print(stored_data)
-    return 'Nothing'
+def print_json(n_clicks, next_node_id, next_edge_id, stored_json):
+    if n_clicks:
+        stored_graph = json.loads(stored_json)
+        output_data = {
+            "next_node_id": next_node_id,
+            "next_edge_id": next_edge_id,
+            "graph": stored_graph
+        }
+        with open('last_data.json','w') as output_file:
+            output_file.write(json.dumps(output_data, indent=2))
+    return ""
 
 
 # Node creation and deletion
@@ -201,12 +236,14 @@ def print_json(n_clicks, stored_json):
     [Input('button-create-node', 'n_clicks_timestamp'),
     Input('button-delete-node', 'n_clicks_timestamp'),
     Input('button-create-edge', 'n_clicks_timestamp'),
-    Input('button-delete-edge', 'n_clicks_timestamp'),],
+    Input('button-delete-edge', 'n_clicks_timestamp'),
+    Input('use-saved-json', 'n_clicks_timestamp'),],
     [State('input-node-name', 'value'),
     State('node-type','value'),
     State('delete-node-id','children'),
     State('input-edge-name', 'value'),
     State('edge-direction', 'value'),
+    State('edge-dotted', 'value'),
     State('source-node-id','children'),
     State('target-node-id','children'),
     State('delete-edge-id','children'),
@@ -214,11 +251,11 @@ def print_json(n_clicks, stored_json):
     State('next-edge-id','children'),
     State('store-data', 'children')]
 )
-def create_node(ts_clicks_create_node, ts_clicks_delete_node, ts_clicks_create_edge, ts_clicks_delete_edge,
+def create_node(ts_clicks_create_node, ts_clicks_delete_node, ts_clicks_create_edge, ts_clicks_delete_edge, ts_clicks_use_saved_json,
     name_create_node, node_type, delete_id_node,
-    name_create_edge, edge_direction, source_id, target_id,
+    name_create_edge, edge_direction, edge_dotted, source_id, target_id,
     delete_id_edge, next_node_id, next_edge_id, stored_json):
-    if button_was_fired(ts_clicks_create_node,[ts_clicks_delete_node, ts_clicks_create_edge, ts_clicks_delete_edge]):
+    if button_was_fired(ts_clicks_create_node,[ts_clicks_delete_node, ts_clicks_create_edge, ts_clicks_delete_edge, ts_clicks_use_saved_json]):
         new_data = [{"data": {'id':next_node_id, 'label': name_create_node}, 'classes': node_type}]
         stored_data = json.loads(stored_json)
         new_stored_data = stored_data + new_data
@@ -226,26 +263,37 @@ def create_node(ts_clicks_create_node, ts_clicks_delete_node, ts_clicks_create_e
         print("Added new {} node".format(node_type))
         new_next_node_id = str(int(next_node_id) + 1)
         return new_stored_json, new_next_node_id, next_edge_id
-    if button_was_fired(ts_clicks_delete_node, [ts_clicks_create_edge, ts_clicks_create_edge, ts_clicks_delete_edge]):
+    if button_was_fired(ts_clicks_delete_node, [ts_clicks_create_edge, ts_clicks_create_edge, ts_clicks_delete_edge, ts_clicks_use_saved_json]):
         stored_data = json.loads(stored_json)
         new_stored_data = clear(stored_data, delete_id_node)
         new_stored_json = json.dumps(new_stored_data)
         print("Node {} deleted".format(delete_id_node))
         return new_stored_json, next_node_id, next_edge_id
-    if button_was_fired(ts_clicks_create_edge,[ts_clicks_delete_edge, ts_clicks_create_node, ts_clicks_delete_node]):
-        new_data = [{"data": {'source':source_id, 'target': target_id, 'id':next_edge_id, 'label': name_create_edge}, 'classes': edge_direction}]
+    if button_was_fired(ts_clicks_create_edge,[ts_clicks_delete_edge, ts_clicks_create_node, ts_clicks_delete_node, ts_clicks_use_saved_json]):
+        new_data = [{"data": {'source':source_id, 'target': target_id, 'id':next_edge_id, 'label': name_create_edge}, 'classes': edge_direction + " " + edge_dotted}]
         stored_data = json.loads(stored_json)
         new_stored_data = stored_data + new_data
         new_stored_json = json.dumps(new_stored_data)
         print("Added new {} edge".format(edge_direction))
         new_next_edge_id = str(int(next_edge_id) - 1)
         return new_stored_json, next_node_id, new_next_edge_id
-    if button_was_fired(ts_clicks_delete_edge, [ts_clicks_create_edge, ts_clicks_create_node, ts_clicks_delete_node]):
+    if button_was_fired(ts_clicks_delete_edge, [ts_clicks_create_edge, ts_clicks_create_node, ts_clicks_delete_node, ts_clicks_use_saved_json]):
         stored_data = json.loads(stored_json)
         new_stored_data = clear(stored_data, delete_id_edge)
         new_stored_json = json.dumps(new_stored_data)
         print("Edge {} deleted".format(delete_id_edge))
         return new_stored_json, next_node_id, next_edge_id
+    if button_was_fired(ts_clicks_use_saved_json, [ts_clicks_create_edge, ts_clicks_create_node, ts_clicks_delete_node, ts_clicks_delete_edge]):
+        try:
+            with open('last_data.json') as input_file:
+                last_json = input_file.read()
+            last_data = json.loads(last_json)
+            graph_json = json.dumps(last_data["graph"])
+            print("Updating graph with last_data.json")
+            return graph_json, last_data["next_node_id"], last_data["next_edge_id"]
+        except FileNotFoundError:
+            print("No saved graph to load")
+            return stored_json, next_node_id, next_edge_id
     return stored_json, "0", "-1"
 
 #Prepare node deletion
